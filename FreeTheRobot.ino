787ba72557942875453a864a936eecfe4769c679
Sylvain Cuomo <zenith1313@gmail.com>
2016-03-25 16:24:08 +0100

    le robot ce deplace tout seul

---
+++ FreeTheRobot.ino
@@ -0,0 +1,75 @@
+#include <Servo.h>
+
+int analogPin = 3; 
+int val = 0;    
+
+Servo RoueDroit;
+Servo RoueGauche;
+
+void setup() {
+  analogReadResolution(8);
+  RoueDroit.attach(6);
+  RoueGauche.attach(5);	
+
+}
+
+
+void Avancer(){
+
+	RoueGauche.write(0);
+	RoueDroit.write(180);
+}
+
+
+void Droite()
+{
+  Right.write(180);
+  Left.write(180);
+
+}
+
+void Gauche();
+{
+  Right.write(0);
+  Left.write(0); 
+}
+
+void CheckDistance(){
+	while (analogRead(analogPin) < 200){
+
+		int v = random(0,20);
+
+	 	if( v  % 2){
+
+	 		 Droite();
+
+	 	} if( v % 5) {
+
+	 		Droite();
+	 		delay(1000);
+
+	 	}else {
+
+	 		Gauche();
+	 	}	
+	}
+}
+
+
+
+void loop{
+
+	 val = analogRead(analogPin);
+	 
+	 if(distance <  200){
+
+	 	CHekDistance();
+
+  	} else {
+
+  		Avancer();
+
+  	}
+}
+
+
