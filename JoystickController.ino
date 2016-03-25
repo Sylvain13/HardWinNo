eab77cebf453d552acc2260fb932d5c92ea4a527
Sylvain Cuomo <zenith1313@gmail.com>
2016-03-25 11:43:45 +0100

    modification commentaire

---
+++ JoystickController.ino
@@ -0,0 +1,95 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include<Servo.h>
+
+Servo Right;
+Servo Left;
+
+#include <Ethernet.h>
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <WiFi101.h>
+#include <BlynkSimpleMKR1000.h>
+
+char auth[] = "81ecfffa0d1f463994e82155129f64b1";
+
+// Your WiFi credentials
+char ssid[] = "topkek";
+char pass[] = "123soleil3";        // Set to "" for open networks
+
+void setup()
+{
+  Right.attach(6);
+  Left.attach(5);
+
+  //Arrêt par défaut
+  Right.write(93);
+  Left.write(90); 
+
+  //Sortie console 9600
+  Serial.begin(9600);
+
+  //Initialisation de la led
+  pinMode(PIN_LED_13, OUTPUT);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void Avancer(){
+
+	Right.write(180);
+	Left.write(0);
+}
+
+void Reculer(){
+
+	Right.write(0);
+  	Left.write(180);
+}
+
+void Droite(){
+
+	Right.write(0);
+  	Left.write(0); 
+
+}
+
+void Gauche(){
+
+  	Right.write(180);
+ 	Left.write(180);
+
+}
+
+BLYNK_WRITE(V8)
+{
+  Right.write(93);
+  Left.write(90); 
+}
+
+
+BLYNK_WRITE(V9){
+		x = param[0].asInt();
+		y = param[1].asInt();	
+		if (  65 < x && x < 195 && 127 < y){
+			Avancer();
+		} else if( 65 < x && x < 195 && y < 127 ){
+			Reculer();
+		} else if( 65 < y && y < 195 && x < 127 ){
+			Gauche();
+		} else if( 65 < x && x < 195 && 127 < y ){
+			Droite();
+		}
+}
+
+void loop()
+{
+  Blynk.run();
+
+}
+
+
