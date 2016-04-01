c4ad230ddf982373ba01a9ba5c3f6a4244e84507
Victor Pedrazzini <victoravans@users.noreply.github.com>
2016-04-01 16:16:53 +0200

    Added files via upload
    Suiveur de ligne

---
+++ Suiveur_de_ligne.ino
@@ -0,0 +1,59 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <Servo.h>
+
+Servo Right;
+Servo Left;
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <WiFi101.h>
+#include <BlynkSimpleMKR1000.h>
+
+char auth[] = "81ecfffa0d1f463994e82155129f64b1";
+
+bool TurnLeft = true;
+
+// Your WiFi credentials
+char ssid[] = "topkek";
+char pass[] = "123soleil3";        // Set to "" for open networks
+
+int CAPTEUR = 0; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_LED_13, OUTPUT);
+  Blynk.begin(auth, ssid, pass);
+  Right.attach(6);
+  Left.attach(5);
+  Right.write(93);
+  Left.write(90);
+}
+
+/////// Bouton d'arrêt d'urgence ///////////
+BLYNK_WRITE(V8)
+{
+  Right.detach();
+  Left.detach();
+}
+
+void loop() {
+
+  Blynk.run();
+  int val = analogRead(CAPTEUR);
+ 
+// ligne blanche détecté
+  if(val < 250) { 
+        Right.write(180);
+        Left.write(90); 
+  }
+// en dehors de la ligne blanche
+  else {
+      Right.write(93);
+      Left.write(0);   
+  }
+}//loop()
